b7083e5ba422f2a0a6495777593dd34f14284b5c
Andrey Zarukin <zarukin38@gmail.com>
2023-10-22 13:23:19 +0800

    comm add func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
 def func_upper(stroka='hello world'):
+    """Выводит все заглавные буквы"""
     print(stroka.upper())
+
+
+def func_title(stroka="good morning people"):
+    """ыводит первые заглавные буквы"""
+    print(stroka.title())
+
